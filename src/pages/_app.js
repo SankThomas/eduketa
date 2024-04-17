bc9ae6fca5ed85d7f4f638f4121ad277338e81f5
sankthomas <thomassankara74@gmail.com>
2024-04-17 15:53:56 +0300

    fix UI hydration error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <Component {...pageProps} />
