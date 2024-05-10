0235906437cd4a4650878f70e92078ec2dfa7840
sankthomas <thomassankara74@gmail.com>
2024-05-10 21:36:46 +0300

    disable code to check for hydration errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,17 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  const [isMounted, setIsMounted] = useState(false);
+  // const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
 
-  if (!isMounted) {
-    return null;
-  }
+  // if (!isMounted) {
+  //   return null;
+  // }
 
   return (
     <>
