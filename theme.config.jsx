47a4c9f4a5a660cae85571ea91dc57615a5c9a9a
SankThomas <thomassankara74@gmail.com>
2024-10-16 11:27:08 +0300

    remove trailing forward slash

--- theme.config.jsx
+++ theme.config.jsx
@@ -8,9 +8,9 @@ export default {
       titleTemplate: "Learnerspree | %s",
     };
   },
-  docsRepositoryBase: "https://github.com/SankThomas/learnerspree/tree/main/",
+  docsRepositoryBase: "https://github.com/SankThomas/learnerspree/tree/main",
   footer: {
-    text: "Copyright Thomas Sankara 2023",
+    text: "Copyright Thomas Sankara 2024",
   },
   chat: {
     link: "https://discord.com/invite/TDr42sFD8g"
