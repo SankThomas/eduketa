6a262fbf083cb4d36f181ca0ec9c93857a5d87ad
SankThomas <thomassankara74@gmail.com>
2024-10-16 11:27:19 +0300

    remove unused code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,7 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-// import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  // const [isMounted, setIsMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
-
-  // if (!isMounted) {
-  //   return null;
-  // }
-
   return (
     <>
       <Component {...pageProps} />
