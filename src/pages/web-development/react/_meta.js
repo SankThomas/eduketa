77b5ff7073c029b02b781fcac3a346c07f6343ba
Thomas Sankara <thomassankara74@gmail.com>
2025-01-02 13:15:54 +0300

    update page structure

--- src/pages/web-development/react/_meta.js
+++ src/pages/web-development/react/_meta.js
@@ -1,4 +1,6 @@
 export default {
   "introduction-to-react": "Introduction to React",
-  "hooks": "React Hooks"
+  "hooks": "React Hooks",
+  "the-virtual-dom": "The Virtual DOM",
+  "react-compiler": "React Compiler"
 }
--- src/pages/web-development/react/hooks/_meta.js
+++ src/pages/web-development/react/hooks/_meta.js
@@ -1,5 +1,12 @@
 export default { 
   "intro": "What are React Hooks",
   "rules-for-writing-hooks": "Rules for writing hooks",
-  "usestate": "The useState Hook",
+  "usestate": "useState",
+  "useeffect": "useEffect",
+  "usecontext": "useContext",
+  "usememo": "useMemo",
+  "usecallback": "useCallback",
+  "useref": "useRef",
+  "usereducer": "useReducer",
+  "custom-hooks": "Custom Hooks"
 }
