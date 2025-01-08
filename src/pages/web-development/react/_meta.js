3a949cad85657b790ae54c66804d2915245be5fc
SankThomas <thomassankara74@gmail.com>
2025-01-08 13:14:28 +0300

    update page structure

--- src/pages/web-development/react/_meta.js
+++ src/pages/web-development/react/_meta.js
@@ -1,6 +1,6 @@
 export default {
   "introduction-to-react": "Introduction to React",
-  "hooks": "React Hooks",
   "the-virtual-dom": "The Virtual DOM",
-  "react-compiler": "React Compiler"
+  "react-compiler": "React Compiler",
+  "hooks": "React Hooks",
 }
